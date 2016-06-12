e5cf215e165966e7e952fba13e1a7b2c4c9dd766
interma <interma@outlook.com>
2016-06-12 16:23:09 +0800

    more c11 example

--- c11/c11_test.cpp
+++ c11/c11_test.cpp
@@ -6,14 +6,41 @@
 
 #include <stdio.h>
 #include <atomic>
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 std::atomic<int> id;
 
-int main(int argc, char **argv)
-{
-	id++;
-	//printf("i am ok[%d]\n", id);
-	printf("i am ok\n");
-	return 0;
+uint64_t gettid() { 
+	return syscall(SYS_gettid); 
 }
+
+void hello(int i, int j){
+	//std::cout << "Hello from thread:"  << i << " tid:" << std::this_thread::get_id() << std::endl; //not thread-safe
+	printf("hello from thread:%d, tid1:%u, tid2:%llu\n", i, std::this_thread::get_id(), gettid());	
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+}
+
+int main(int argc, char **argv) {
+    std::vector<std::thread> threads;
+
+    for(int i = 0; i < 5; ++i){
+        threads.push_back(std::thread(hello, i, i));
+    }
+
+    for(auto& thread : threads){
+        thread.join();
+    }
+
+	std::cout<<"main exit"<<std::endl;
+    return 0;
+}
+
+
 
---
+++ c11/printf_type.cc
@@ -0,0 +1,18 @@
+/*
+ desc: 	
+ author: interma (interma@outlook.com)
+ created: 06/09/16 22:40:39 HKT
+*/
+#include <stdio.h>
+#include <stdint.h>
+
+//typedef unsigned long long 
+
+int main()
+{
+	printf("uint32_t:%d\n", sizeof(uint32_t));
+	printf("uint64_t:%d\n", sizeof(uint64_t));
+	printf("unsigned long long:%d\n", sizeof(unsigned long long));
+	printf("unsigned long:%d\n", sizeof(unsigned long));
+	return 0;
+}
