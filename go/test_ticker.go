dcc5144e1c19f5d23ef2fd4806f60ce4c4e46240
interma <interma@outlook.com>
2016-08-11 18:28:21 +0800

    add go test

---
+++ go/test_ticker.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "time"
+    "log"
+)
+
+func test1() {
+    ticker := time.NewTicker(1 * time.Second)
+    go func(){
+        for _ = range ticker.C {
+            log.Println("tick")
+        }
+        log.Println("stopped")
+    }()
+    time.Sleep(3 * time.Second)
+    log.Println("stopping ticker")
+    ticker.Stop() //don't close C
+    time.Sleep(3 * time.Second)
+}
+func test2() {
+	//confuse follow code
+    ticker := time.NewTicker(1 * time.Second)
+    go func(){
+		time.Sleep(3 * time.Second)
+        for _ = range ticker.C {
+            log.Println("tick")
+        }
+        log.Println("stopped")
+    }()
+    time.Sleep(6 * time.Second)
+    log.Println("stopping ticker")
+    ticker.Stop() //don't close C
+}
+func test3() {
+	//this is enough from my raft 
+    log.Println("timer seted!") 
+	timer := time.NewTimer(2*time.Second) 
+    time.Sleep(1 * time.Second)
+	timer.Reset(2*time.Second)
+	go func(){
+		<- timer.C 
+		log.Println("timer expired!")
+	}()
+    time.Sleep(4 * time.Second)
+}
+
+
+func main() {
+	test3()
+}
