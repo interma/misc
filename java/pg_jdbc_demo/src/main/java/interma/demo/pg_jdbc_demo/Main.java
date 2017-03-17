13a3a5323a4725e8026e893bb6aa4ab7ffc60b84
interma <interma@outlook.com>
2017-03-17 14:42:32 +0800

    add java maven example

---
+++ java/pg_jdbc_demo/src/main/java/interma/demo/pg_jdbc_demo/Main.java
@@ -0,0 +1,11 @@
+package interma.demo.pg_jdbc_demo;
+
+/**
+ * Created by hma on 17/03/2017.
+ */
+public class Main {
+    public static void main(String[] argv) throws Exception {
+        System.out.printf("demo\n");
+    }
+
+}
