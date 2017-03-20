40d813f94a37bfe9313a9003f5ff94dfe01c9583
interma <interma@outlook.com>
2017-03-20 17:02:08 +0800

    add more default params of python

--- py/common_use.py
+++ py/common_use.py
@@ -2,11 +2,12 @@ import sys
 
 
 #default params
-def foo(word, who = "interma"):
-    print "%s say: %s" % (who, word)
+def foo(word, who = "interma", times = 1):
+    for i in range(0,times):
+        print "%s say: %s" % (who, word)
 
 
 if __name__ == "__main__":
-    foo("hello", "I")
+    foo("hello", times = 2, who = "I")
     foo("hello")
     sys.exit(0)
