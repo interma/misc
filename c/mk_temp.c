b67eaa6466f2805dbcf838f2d5526a4f99907a61
interma <interma@outlook.com>
2017-04-06 13:58:31 +0800

    add another method mk_temp

--- c/mk_temp.c
+++ c/mk_temp.c
@@ -7,13 +7,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-int main() 
+void foo1()
 {
 	char template[] = "/tmp/my_folder/fileXXXXXX"; //my_folder need created manually
 	char content[] = "xxx\nyyy";
 	int fd = mkstemp(template);
 	write(fd, content, strlen(content));
 	close(fd);
+}
+
+void foo2()
+{
+	char template[] = "/tmp/my_folder/fileXXXXXX"; //my_folder need created manually
+	char content[] = "xxx\nyyy";
+	const char *file_name = mktemp(template);
+	int fd = open(file_name, O_CREAT | O_TRUNC | O_WRONLY);
+	write(fd, content, strlen(content));
+	close(fd);
+}
+
+int main() 
+{
+	foo2();
 }
 
