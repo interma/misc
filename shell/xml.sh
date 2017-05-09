854dcd5f499ce6f2517024b49fe04e7863d5f7c4
interma <interma@outlook.com>
2017-05-09 12:08:57 +0800

    add shell xml example

---
+++ shell/xml.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+
+value=''
+
+function get_value_bytag() {
+	f=$1
+	tag=$2
+	value=`cat $f | tr '\n' ' ' | awk -F '<property>' '{ for(i = 1; i <= NF; i++) { print $i; } }' | grep $tag | sed -n 's|.*<value>\(.*\)</value>.*|\1|p'`
+}
+
+get_value_bytag test.xml hawq_master_address_host
+echo $value
