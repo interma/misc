cf4aea794de5faa9e5cb4319d57151c72c28cfb5
interma <interma@outlook.com>
2017-05-10 11:02:45 +0800

    test shell return val with parameters

--- shell/xml.sh
+++ shell/xml.sh
@@ -7,7 +7,11 @@ function get_value_bytag() {
 	f=$1
 	tag=$2
 	value=`cat $f | tr '\n' ' ' | awk -F '<property>' '{ for(i = 1; i <= NF; i++) { print $i; } }' | grep $tag | sed -n 's|.*<value>\(.*\)</value>.*|\1|p'`
+	echo $value
+	#echo "XXX"
 }
 
-get_value_bytag test.xml hawq_master_address_host
-echo $value
+#get_value_bytag test.xml hawq_master_address_host
+#echo $value
+val=$(get_value_bytag test.xml hawq_master_address_host)
+echo $val
